b47cdfc6e0e08264b0f8f80f0c0cd6ab99433f61
Nix-Banayat <164652107+Nix-Banayat@users.noreply.github.com>
2024-06-11 18:06:17 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,80 +1,54 @@
-﻿namespace IdentifyAge
+﻿using System;
+
+namespace IdentifyAge
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO: Simplify
             Console.Write("Enter year of birth: ");
             int yearOfBirth = Convert.ToInt16(Console.ReadLine());
 
+            int age = AgeCalculator(yearOfBirth);
+            string generation = GettingGeneration(age);
+            string stage = GettingStage(age);
+
+            Console.WriteLine($"You are {age} years old, {generation}, {stage}");
+        }
+
+        public static int AgeCalculator(int yearOfBirth)
+        {
             int age = 2024 - yearOfBirth;
-            string generation = "";
-            string stage = ""; 
+            string generation = GettingGeneration(age);
+            string stage = GettingStage(age);
 
-            if (age >= 79 && age <= 96)
-            {
-                generation = "The Silent Generation";
-            }
-            else if (age >= 60 && age <= 78)
-            {
-                generation = "Baby Boomers";
-            }
-            else if (age >= 44 && age <= 59)
-            {
-                generation = "Gen X";
-            }
-            else if (age >= 28 && age <= 43)
-            {
-                generation = "Millenials";
-            }
-            else if (age >= 12 && age <= 27)
-            {
-                generation = "Gen Z";
-            }
-            else if (age >= 0 && age <= 11)
-            {
-                generation = "Gen Alpha";
-            }
+            return age;
 
-            if (age >= 0 && age <= 3)
-            {
-                stage = "Infant";
-            }
-            else if (age >= 4 && age <= 6)
-            {
-                stage = "Early Childhood";
-            }
-            else if (age == 7 || age == 8)
-            {
-                stage = "Middle Childhood";
-            }
-            else if (age >= 9 && age <= 11)
-            {
-                stage = "Late Childhood";
-            }
-            else if (age >= 12 && age <= 20)
-            {
-                stage = "Adolescence";
-            }
-            else if (age >= 21 && age <= 35)
-            {
-                stage = "Early Adulthood";
-            }
-            else if (age >= 36 && age <= 50)
-            {
-                stage = "Midlife";
-            }
-            else if (age >= 51 && age <= 80)
-            {
-                stage = "Mature Adulthood";
-            }
-            else if (age > 80)
-            {
-                stage = "Late Adulthood";
-            }
+        }
+        public static string GettingGeneration(int age)
+        {
+            string generation = age >= 79 && age <= 96 ? "The Silent Generation" :
+                         age >= 60 && age <= 78 ? "Baby Boomers" :
+                         age >= 44 && age <= 59 ? "Gen X" :
+                         age >= 28 && age <= 43 ? "Millenials" :
+                         age >= 12 && age <= 27 ? "Gen Z" :
+                         age >= 0 && age <= 11 ? "Gen Alpha" : "";
 
-            Console.WriteLine($"{age} years old, {generation}, {stage}");
+            return generation;
+        }
+        public static string GettingStage(int age)
+        {
+            string stage = age >= 0 && age <= 3 ? "Infant" :
+                           age >= 4 && age <= 6 ? "Early Childhood" :
+                           age == 7 || age == 8 ? "Middle Childhood" :
+                           age >= 9 && age <= 11 ? "Late Childhood" :
+                           age >= 12 && age <= 20 ? "Adolescence" :
+                           age >= 21 && age <= 35 ? "Early Adulthood" :
+                           age >= 36 && age <= 50 ? "Midlife" :
+                           age >= 51 && age <= 80 ? "Mature Adulthood" :
+                           age > 80 ? "Late Adulthood" : "";
+
+            return stage;
         }
     }
 }
